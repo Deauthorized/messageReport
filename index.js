1172f42ffd31416e7cbf76fd621f6b519cd0f9c0
Deauthorized <49701242+Deauthorized@users.noreply.github.com>
2022-01-14 13:13:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,18 +11,16 @@ module.exports = function({ bot, knex, config, commands }) {
 
     const appId = bot.application.id;
 
-    bot.on("ready", () => {
-      bot.createCommand(config["mainServerId"], {
-        name: "Report Message",
-        description: "Start a new thread with this message.",
-        type: 3
-      })
-        .then(cmd => {
-          log("Application command published to main server successfully.")
-        })
-  
-        .catch(err => {
-          error("Failed to publish application command. This usually means that I do not have permissions to create guild commands in the main server. Unfortunately, the only way to correct this problem is to kick the bot and re-invite it with the `application.commands` scope.")
-        })
+    bot.createCommand(config["mainServerId"], {
+      name: "Report Message",
+      description: "Report this message.",
+      type: 3
     })
+      .then(cmd => {
+        log("Application command published to main server successfully.")
+      })
+
+      .catch(err => {
+        error("Failed to publish application command. This usually means that I do not have permissions to create guild commands in the main server. Unfortunately, the only way to correct this problem is to kick the bot and re-invite it with the `application.commands` scope.")
+      })
   }
