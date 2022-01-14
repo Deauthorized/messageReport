aa3675e0d54655719a148103eb977ef45ab93229
Deauthorized <49701242+Deauthorized@users.noreply.github.com>
2022-01-14 13:27:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 module.exports = function({ bot, knex, config, commands }) {
-    function log(log) {
-      console.info(`[messageReport:log] ${log}`)
-    }
-
-    function err(err) {
-      console.error(`[messageReport:error] ${err}`)
-    }
-
-    log("Initializing...")
-
-    const appId = bot.application.id;
-
-    console.log(bot.guilds)
-
-    console.log(config.mainServerId[0])
-
-    bot.guilds.get(config.mainServerId[0]).createCommand({
-      name: "Report Message",
-      description: "Report this message.",
-      type: 3
-    })
-      .then(cmd => {
-        log("Application command published to main server successfully.")
-      })
-
-      .catch(error => {
-        err("Failed to publish application command. This usually means that I do not have permissions to create guild commands in the main server. Unfortunately, the only way to correct this problem is to kick the bot and re-invite it with the `application.commands` scope.")
-        err(error)
-        return;
-      })
+  function log(log) {
+    console.info(`[messageReport:log] ${log}`)
   }
+
+  function err(err) {
+    console.error(`[messageReport:error] ${err}`)
+  }
+
+  log("Initializing...")
+  
+  bot.guilds.get(config.mainServerId[0]).createCommand({
+    name: "Report Message",
+    type: 3
+  })
+    .then(cmd => {
+      log("Application command published to main server successfully.")
+    })
+
+    .catch(error => {
+      err("Failed to publish application command. This usually means that I do not have permissions to create guild commands in the main server. Unfortunately, the only way to correct this problem is to kick the bot and re-invite it with the `application.commands` scope.")
+      err(error)
+      return;
+    })
+}
