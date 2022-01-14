d5bee9c9111b99eeaad06a3c6d8a1bb8a88199be
Deauthorized <49701242+Deauthorized@users.noreply.github.com>
2022-01-14 12:24:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 module.exports = function({ bot, knex, config, commands }) {
-    //soon
+    function log(log) {
+      console.info(`[messageReport] ${log}`)
+    }
+
+    log("Initializing...")
   }
