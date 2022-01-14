885f001b6712122fc5c61301915883aa7402d753
Deauthorized <49701242+Deauthorized@users.noreply.github.com>
2022-01-14 12:50:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 module.exports = function({ bot, knex, config, commands }) {
     function log(log) {
-      console.info(`[messageReport] ${log}`)
+      console.info(`[messageReport:log] ${log}`)
+    }
+
+    function err(err) {
+      console.error(`[messageReport:error] ${err}`)
     }
 
     log("Initializing...")
+
+    const appId = config["mr.appId"];
+
+    if (!appId) {
+      err("An Application ID is required to publish my interaction commands.")
+    }
+
+    log(`Configured Application ID is: ${config["mr.appId"].substring(0,10)}********`)
   }
