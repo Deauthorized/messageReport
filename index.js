77d4b09e72da612505de35aaeaf4658956e1d12b
Deauthorized <49701242+Deauthorized@users.noreply.github.com>
2022-01-15 14:33:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 module.exports = function({ bot, knex, config, commands, threads }) {
   function log(log) {
-    console.info(`[messageReport:log] ${log}`)
+    console.info(`[messageReport:log] ${log}`);
   }
 
   function err(err) {
-    console.error(`[messageReport:error] ${err}`)
+    console.error(`[messageReport:error] ${err}`);
   }
 
   async function isBlocked(userId) {
     const row = await knex("blocked_users").where("user_id", userId).first();
-    return !!row;
+    return row;
   }
 
-  log("Initializing...")
+  log("Initializing...");
 
   bot.guilds.get(config.mainServerId[0]).createCommand({
     name: "Report Message",
@@ -23,8 +23,8 @@ module.exports = function({ bot, knex, config, commands, threads }) {
     })
 
     .catch(error => {
-      err("Fatal: Failed to publish the Report Message interaction in the main server. Unfortunately, the only way to correct this problem is to kick the bot and re-invite it with the `application.commands` scope.")
-      err(error)
+      err("Fatal: Failed to publish the Report Message interaction in the main server. Unfortunately, the only way to correct this problem is to kick the bot and re-invite it with the `application.commands` scope.");
+      err(error);
       return;
     })
 
@@ -34,21 +34,27 @@ module.exports = function({ bot, knex, config, commands, threads }) {
 
     await i.acknowledge(64)
 
-    const reportMsg = i.data.resolved.messages.random()
-
     if (await isBlocked(i.member.id)) {
       const row = await knex("blocked_users").where("user_id", i.member.id).first();
       await i.createFollowup( { content: `**You are currently blocked from creating threads.** This block ${(row.expires_at == null ? "has not been set to expire automatically." : `will expire <t:${Math.round(Date.parse(row.expires_at + " UTC") / 1000)}:R>.`)}` } );
       return;
     }
 
-    if (await threads.findOpenThreadByUserId(i.member.id)) {
-      await i.createFollowup( { content: "You already have an active thread." } )
+    const reportMsg = i.data.resolved.messages.random()
+
+    const msgModel = `:page_with_curl: **Reported Message** (https://discord.com/channels/${reportMsg.guildID}/${reportMsg.channel.id}/${reportMsg.id})\n\n**${reportMsg.author.username}#${reportMsg.author.discriminator} => <#${reportMsg.channel.id}>:** ${(reportMsg.cleanContent.substring(0, 300).length == 0 ? "[no content]" : reportMsg.cleanContent.substring(0, 300))}`
+
+    if (await threads.findOpenThreadByUserId(i.member.id), t => {
+      if (t) {
+        t.postSystemMessage(msgModel);
+        await i.createFollowup( { content: "**Message added to your active report.**" } );
+        return;
+      }
       return;
-    }
+    })
 
     if (reportMsg.type !== 0) {
-      await i.createFollowup( { content: "This type of message cannot be reported." } )
+      await i.createFollowup( { content: "This type of message cannot be reported." } );
       return;
     }
 
@@ -57,8 +63,8 @@ module.exports = function({ bot, knex, config, commands, threads }) {
       categoryId: config.categoryAutomation.newThread
     })
       .then(nt => {
-        nt.postSystemMessage(`:gear: **Message Report** (https://discord.com/channels/${reportMsg.guildID}/${reportMsg.channel.id}/${reportMsg.id})\n\n**${reportMsg.author.username}#${reportMsg.author.discriminator} => <#${reportMsg.channel.id}>:** ${(reportMsg.cleanContent.substring(0, 300).length == 0 ? "[no content]" : reportMsg.cleanContent.substring(0, 300))}`)
-        i.createFollowup( {content: (!config.mr["reportResponseMessage"] ? "Thank you! A modmail thread has been created with this message attached." : config.mr["reportResponseMessage"])} )
+        nt.postSystemMessage(msgModel);
+        i.createFollowup( {content: (!config.mr["reportResponseMessage"] ? "Thank you! A modmail thread has been created with this message attached." : config.mr["reportResponseMessage"])} );
       })
 
   })
