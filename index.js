e177909ff22e55aecb822b423a008f272138e8c2
Deauthorized <49701242+Deauthorized@users.noreply.github.com>
2022-01-15 23:39:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -42,8 +42,6 @@ module.exports = function({ bot, knex, config, commands, threads }) {
 
     const msgModel = `:page_with_curl: **${i.member.username}** attached a message: (https://discord.com/channels/${reportMsg.guildID}/${reportMsg.channel.id}/${reportMsg.id})\n\n**${reportMsg.author.username}#${reportMsg.author.discriminator} => <#${reportMsg.channel.id}>:** ${(reportMsg.cleanContent.substring(0, 300).length == 0 ? "[no content]" : reportMsg.cleanContent.substring(0, 300))}`
 
-    console.log(await threads.findOpenThreadByUserId(i.member.id));
-
     if (await threads.findOpenThreadByUserId(i.member.id)){
       const t = await threads.findOpenThreadByUserId(i.member.id)
       await t.postSystemMessage(msgModel);
